6cb09b338e87f7a0a810336ea4d184a25f5f2a3f
junsan <xjunsantos@gmail.com>
2024-04-04 05:45:58 +0800

    user route and logout

--- resources/views/frontend/dashboard/layouts/sidebar.blade.php
+++ resources/views/frontend/dashboard/layouts/sidebar.blade.php
@@ -11,7 +11,12 @@
           <li><a href="dsahboard_review.html"><i class="far fa-star"></i> Reviews</a></li>
           <li><a href="dsahboard_wishlist.html"><i class="far fa-heart"></i> Wishlist</a></li>
           <li><a href="dsahboard_profile.html"><i class="far fa-user"></i> My Profile</a></li>
-          <li><a href="dsahboard_address.html"><i class="fal fa-gift-card"></i> Addresses</a></li>
-          <li><a href="#"><i class="far fa-sign-out-alt"></i> Log out</a></li>
+          <li><a href="dsahboard_address.html"><i class="fal fa-gift-card"></i> Addresses</a></li>  
+            <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <li><a href="{{route('logout')}}" onclick="event.preventDefault();
+                                        this.closest('form').submit();"><i class="far fa-sign-out-alt"></i> Log out</a></li>
+                 
+            </form>
         </ul>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Frontend\UserDashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +21,12 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('frontend.dashboard.dashboard');
+    
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'user', 'as' => 'user.'], function() {
+    Route::get('dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
