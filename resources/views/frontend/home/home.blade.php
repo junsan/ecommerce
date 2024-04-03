40895b94a744272188d34d255bc4e9e62636f012
junsan <xjunsantos@gmail.com>
2024-04-04 04:21:30 +0800

    login and register

--- resources/views/frontend/home/home.blade.php
+++ resources/views/frontend/home/home.blade.php
@@ -133,7 +133,7 @@
                 </div>
             </div>
         </div>
-    </section>
+</section>
     <!--==========================
       PRODUCT MODAL VIEW END
     ===========================-->
