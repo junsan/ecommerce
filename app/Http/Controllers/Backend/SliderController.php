f0d89ac7dc6f616c1afbbeb8435aa0ba327d65f2
junsan <xjunsantos@gmail.com>
2024-04-04 16:32:50 +0800

    slider create

---
+++ app/Http/Controllers/Backend/SliderController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Slider;
+
+class SliderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('admin.slider.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.slider.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'banner' => 'image',
+            'type' => 'required|max:255',
+            'text' => 'required|max:255',
+            'starting_price' => 'required|max:255',
+            'button_url' => 'required|url',
+            'serial' => 'required|integer',
+            'status' => 'required'
+        ]);
+
+        $slider = new Slider();
+        $slider->type = $request->type;
+        $slider->text = $request->text;
+        $slider->starting_price = $request->starting_price;
+        $slider->button_url = $request->button_url;
+        $slider->serial = $request->serial;
+        $slider->status = $request->status;
+        $slider->save();
+
+        toastr('Slider has been created successfully.');
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/slider/create.blade.php
@@ -0,0 +1,67 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <!-- Main Content -->
+        <section class="section">
+          <div class="section-header">
+            <h1>Slider</h1>
+            <div class="section-header-breadcrumb">
+              <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+              <div class="breadcrumb-item"><a href="#">Components</a></div>
+              <div class="breadcrumb-item">Slider</div>
+            </div>
+          </div>
+
+          <div class="section-body">
+            <div class="row">
+              <div class="col-12 col-md-12 col-lg-12">
+                <div class="card">
+                  <div class="card-header">
+                    <h4>Create Slider</h4>
+                    <div class="card-header-action">
+                        <a href="" class="btn btn-light">Back</a>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <form action="{{route('admin.slider.store')}}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group">
+                      <label>Banner</label>
+                      <input name="banner" type="file" class="form-control">
+                    </div>
+                    <div class="form-group">
+                      <label>Type</label>
+                      <input name="type" value="{{old('type')}}" type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                      <label>Text</label>
+                      <input name="text" value="{{old('text')}}" type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                      <label>Starting Price</label>
+                      <input name="starting_price" value="{{old('starting_price')}}" type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                      <label>Button Url</label>
+                      <input name="button_url" value="{{old('button_url')}}" type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                      <label>Serial</label>
+                      <input name="serial" value="{{old('serial')}}" type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                      <label>Status</label>
+                      <select name="status" class="form-control form-control-sm">
+                        <option value="1">Active</option>
+                        <option value="0">Inactive</option>
+                      </select>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+@endsection
